37230b5125e9c10ea2b2a116016810d5b13a2f5f
vinay kumar sharma <102848826+vinaysharma1228@users.noreply.github.com>
2024-02-17 09:59:43 +0530

    Time: 24 ms (76.75%), Space: 59.3 MB (56.44%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -1,35 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i += 3) {
-            // Check if current array can be formed
-            if (i + 2 < nums.length && nums[i + 2] - nums[i] <= k) {
-                List<Integer> currentArray = new ArrayList<>();
-                currentArray.add(nums[i]);
-                currentArray.add(nums[i + 1]);
-                currentArray.add(nums[i + 2]);
-                result.add(currentArray);
-            } else {
-                // If not, return empty array
-                return new int[][]{};
-            }
+        int[][] result = new int[nums.length/3][3];
+        
+        for (int i=0,j=0; i< nums.length; i+=3, j++){
+            if (nums[i+2]-nums[i]>k) return new int[0][0];
+            result[j][0] = nums[i];
+            result[j][1] = nums[i+1];
+            result[j][2] = nums[i+2];
         }
-
-        // Convert List<List<Integer>> to int[][]
-        int[][] ret = new int[result.size()][3];
-        for (int i = 0; i < result.size(); i++) {
-            for (int j = 0; j < 3; j++) {
-                ret[i][j] = result.get(i).get(j);
-            }
-        }
-
-        return ret;
+        return result;
     }
 }
