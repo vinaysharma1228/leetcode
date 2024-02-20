bbe24f365a53a4ef1b254d01f0f7903276887464
vinay kumar sharma <102848826+vinaysharma1228@users.noreply.github.com>
2024-02-20 16:59:43 +0530

    Time: 2 ms (89.71%), Space: 45 MB (20.33%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,62 @@
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        solveNQueensHelper(board, 0, result);
+        return result;
+    }
+
+    private void solveNQueensHelper(char[][] board, int row, List<List<String>> result) {
+        if (row == board.length) {
+            result.add(constructBoard(board));
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isValidMove(board, row, col)) {
+                board[row][col] = 'Q';
+                solveNQueensHelper(board, row + 1, result);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isValidMove(char[][] board, int row, int col) {
+        // Check if there is a queen in the same column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check if there is a queen in the upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check if there is a queen in the upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> constructBoard(char[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            solution.add(new String(board[i]));
+        }
+        return solution;
+    }
+}
