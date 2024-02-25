cc6cc3315994c88729c6f93b250ea8d18eafe229
vinay kumar sharma <102848826+vinaysharma1228@users.noreply.github.com>
2024-02-25 11:52:02 +0530

    Time: 16 ms (98.18%), Space: 57.5 MB (50.91%) - LeetHub

--- 0100-greatest-common-divisor-traversal/0100-greatest-common-divisor-traversal.java
+++ 0100-greatest-common-divisor-traversal/0100-greatest-common-divisor-traversal.java
@@ -1,66 +1,59 @@
 class Solution {
-    public boolean canTraverseAllPairs(int[] nums) {
-        if(nums.length == 1) return true;
-        int n = nums.length;
-        int maxElement = Arrays.stream(nums).max().getAsInt();
-        if(Arrays.stream(nums).min().getAsInt() == 1) return false;
-        int[] factorArray = factorsCalculator(maxElement);
-        
-        int[] parent = new int[maxElement + 1];
-        int[] rank = new int[maxElement + 1];
-        for (int i = 0; i < parent.length; i++) {
-            parent[i] = i;
-            rank[i] = 1;
-        }
 
-        for (int num : nums) {
-            int x = num;
-            while (x > 1) {
-                int p = factorArray[x];
-                union(parent, rank, p, num);
-                while (x % p == 0) {
-                    x = x / p;
+    int[] parent;
+    int[] count;
+
+    int max;
+    public boolean canTraverseAllPairs(int[] nums) {
+        for (int x : nums) 
+            max = Math.max(max, x);
+
+        int n = nums.length;
+        parent = new int[max +1];
+        count = new int[max+1];
+
+        if(n == 1) return true;
+
+        for(int i = 0; i < n; i++)
+            if(nums[i]==1) return false;
+        
+        for(int i = 1; i <= max; i++)
+            parent[i] = i;
+        
+        for(int x:nums)
+            count[x]++;
+        
+
+        boolean[] visited = new boolean[max+1];
+
+        for(int i = 2; i* 2 <= max; i++){
+            if(visited[i]) continue;
+            for(int j = i+i; j <= max; j+=i){
+                visited[j] = true;
+                if(count[j]!=0){
+                    union(i, j);
                 }
             }
         }
 
-        int p = find(parent, nums[0]);
-        for (int i = 1; i < nums.length; i++) {
-            if (find(parent, nums[i]) != p) return false;
-        }
-
+        int p = find(nums[0]);
+        for(int i = 1; i < n; i++)
+            if(find(nums[i]) != p) return false;
+        
         return true;
     }
 
-    private int[] factorsCalculator(int n) {
-        int[] dp = new int[n + 2];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = i;
-        }
-        for (int i = 2; i <= n; i++) {
-            if (dp[i] == i) {
-                for (int j = i * 2; j <= n; j += i) {
-                    if (dp[j] == j) dp[j] = i;
-                }
-            }
-        }
-        return dp;
+    private int find(int x) {
+        return parent[x] == x ? x : (parent[x] = find(parent[x]));
     }
+    
+    private void union(int x, int y) {
+        
+        x = find(x);
+        y = find(y);
+        if (x == y) 
+            return;
 
-    private int find(int[] parent, int a) {
-        return parent[a] = parent[a] == a ? a : find(parent, parent[a]);
-    }
-
-    private void union(int[] parent, int[] rank, int a, int b) {
-        a = find(parent, a);
-        b = find(parent, b);
-        if (a == b) return;
-        if (rank[a] < rank[b]) {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-        parent[b] = a;
-        rank[a] += rank[b];
+        parent[y] = x;
     }
 }
