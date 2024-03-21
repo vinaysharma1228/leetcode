5912f45b71157fd6e228e84171a40061bb93a625
vinay kumar sharma <102848826+vinaysharma1228@users.noreply.github.com>
2024-03-21 09:05:38 +0530

    Time: 2 ms (100%), Space: 44.4 MB (91.91%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -1,41 +1,17 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
-        // Create a HashMap to store the count of each element in the first array
-        Map<Integer, Integer> countMap = new HashMap<>();
-        for (int num : nums[0]) {
-            countMap.put(num, 1);
+        int freq[]=new int[1001];
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums[i].length;j++){
+              freq[nums[i][j]]++;
+            }
+           
         }
-
-        // Iterate through the remaining arrays and update the count of common elements
-        for (int i = 1; i < nums.length; i++) {
-            countMap = updateCountMap(countMap, nums[i]);
-        }
-
-        // Create a List to store the common elements and add them to the list
-        List<Integer> result = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            if (entry.getValue() == nums.length) {
-                result.add(entry.getKey());
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==nums.length){
+                a.add(i);
             }
         }
-
-        // Sort the result list in ascending order
-        Collections.sort(result);
-        return result;
-    }
-
-    private Map<Integer, Integer> updateCountMap(Map<Integer, Integer> countMap, int[] nums) {
-        // Create a temporary HashMap to store the updated count
-        Map<Integer, Integer> temp = new HashMap<>();
-
-        // Iterate through the current array and update the count of common elements
-        for (int num : nums) {
-            if (countMap.containsKey(num)) {
-                temp.put(num, countMap.get(num) + 1);
-            }
-        }
-
-        // Return the updated count map
-        return temp;
-    }
-}
+        return a;
+    }}
