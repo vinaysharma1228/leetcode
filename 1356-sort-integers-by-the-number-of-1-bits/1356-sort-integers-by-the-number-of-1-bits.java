38ad7eddfce4be01cfcec4d1c569de855d014426
vinay kumar sharma <102848826+vinaysharma1228@users.noreply.github.com>
2024-03-25 09:28:59 +0530

    Time: 16 ms (17.93%), Space: 44.9 MB (11.39%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,39 @@
+
+
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+            list.add(num);
+        }
+
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                int countA = countOnes(a);
+                int countB = countOnes(b);
+                if (countA == countB) {
+                    return a.compareTo(b); // Sort by value if count of 1's is the same
+                }
+                return countA - countB; // Sort by count of 1's
+            }
+
+            private int countOnes(int num) {
+                int count = 0;
+                while (num != 0) {
+                    if ((num & 1) == 1) {
+                        count++;
+                    }
+                    num = num >>> 1; // Unsigned right shift
+                }
+                return count;
+            }
+        });
+
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+}
